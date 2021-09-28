171a272eac0861ca2d4ad0a7daff084c9ce02068
luoChen <hank.luo@xianghuanji.com>
2021-09-28 18:08:55 +0800

    还访问不到 接口

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
@@ -1,6 +1,7 @@
 package com.qingcheng.controller.goods;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BrandController {
 
-    @RequestMapping("/demo")
+    @GetMapping("/demo")
     public String demo() {
         return "hhjhhh";
     }
