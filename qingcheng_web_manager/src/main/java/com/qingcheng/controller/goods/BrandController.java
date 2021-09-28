c64d0bb66eb5a58f2ea4156e5d66cadb0004e011
luoChen <hank.luo@xianghuanji.com>
2021-09-28 17:27:30 +0800

    还访问不到 接口

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
@@ -1,5 +1,15 @@
 package com.qingcheng.controller.goods;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/brand")
+@RestController
 public class BrandController {
+
+    @RequestMapping("/demo")
+    public String demo() {
+        return "hhjhhh";
+    }
 }
