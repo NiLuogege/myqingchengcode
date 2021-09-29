69c6ac6054d95fe48b1892273d53597bdc8ab4d5
luoChen <hank.luo@xianghuanji.com>
2021-09-29 14:24:51 +0800

    添加 FastJsonHttpMessageConverter 用来转换 request 和 response 的数据

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/goods/BrandController.java
@@ -1,12 +1,15 @@
 package com.qingcheng.controller.goods;
 
 
+import com.qingcheng.pojo.goods.Brand;
 import com.qingcheng.service.goods.BrandService;
 import com.qingcheng.service.impl.BrandServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/brand")
 @RestController
@@ -24,13 +27,13 @@ public class BrandController {
     }
 
     @RequestMapping("/allBrand")
-    public String getAllBrand(){
-        return brandService.getAllBrand().toString();
+    public List<Brand> getAllBrand(){
+        return brandService.getAllBrand();
     }
 
 
     @RequestMapping("/findAll")
-    public String findAll(){
-        return brandService.findAll().toString();
+    public List<Brand> findAll(){
+        return brandService.findAll();
     }
 }
