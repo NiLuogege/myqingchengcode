97cbf3f88ed49572ce3efe164b32dfe73af5cc06
luoChen <hank.luo@xianghuanji.com>
2021-09-30 13:36:51 +0800

    接入 oss

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
@@ -1,5 +1,6 @@
 package com.qingcheng.controller.file;
 
+import com.aliyun.oss.OSSClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("upload")
@@ -17,6 +19,9 @@ public class UploadController {
 
     @Autowired
     private HttpServletRequest request;
+
+    @Autowired
+    private OSSClient ossClient;
 
     @PostMapping("/native")
     public String nativeUpload(@RequestParam("file") MultipartFile file) throws IOException {
@@ -30,6 +35,14 @@ public class UploadController {
         }
         file.transferTo(desFile);
         return "http://localhost:8989/img/" + file.getOriginalFilename();
+    }
+
+    @PostMapping("/oss")
+    public String ossUpload(@RequestParam("file") MultipartFile file,String floder) throws IOException {
+        String bucketName="qingchengdianshang";
+        String fileName = floder+File.separator+ UUID.randomUUID()+file.getOriginalFilename();
+        ossClient.putObject(bucketName,fileName,file.getInputStream());
+        return "https://"+bucketName+".oss-cn-beijing.aliyuncs.com/"+fileName;
     }
 
 }
