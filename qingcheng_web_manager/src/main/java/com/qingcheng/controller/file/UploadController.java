eec0d4040a72cb927ac68b997084bc6caf312f78
luoChen <hank.luo@xianghuanji.com>
2021-09-30 12:02:37 +0800

    文件上传到服务器本机ok

--- qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
+++ qingcheng_web_manager/src/main/java/com/qingcheng/controller/file/UploadController.java
@@ -1,13 +1,35 @@
 package com.qingcheng.controller.file;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 
 @RestController
-@RequestMapping("uplod")
+@RequestMapping("upload")
 public class UploadController {
 
+    @Autowired
+    private HttpServletRequest request;
 
-//    public String
+    @PostMapping("/native")
+    public String nativeUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        System.out.println("native......");
+        //找到真正要存储的位置
+        String path = request.getSession().getServletContext().getRealPath("img");
+        String filePath = path + File.separator + file.getOriginalFilename();
+        File desFile = new File(filePath);
+        if (!desFile.getParentFile().exists()) {
+            desFile.mkdirs();
+        }
+        file.transferTo(desFile);
+        return "http://localhost:8989/img/" + file.getOriginalFilename();
+    }
 
 }
