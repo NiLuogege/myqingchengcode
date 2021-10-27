729f6c1a50c003d4e07ff9ab9f617bf8643e5f4a
luoChen <hank.luo@xianghuanji.com>
2021-10-27 15:33:22 +0800

    添加 秒杀订单

--- qingcheng_service_seckill/src/main/java/com/qingcheng/consumer/SeckillOrderMessageListener.java
+++ qingcheng_service_seckill/src/main/java/com/qingcheng/consumer/SeckillOrderMessageListener.java
@@ -1,7 +1,9 @@
 package com.qingcheng.consumer;
 
 import com.alibaba.fastjson.JSON;
+import com.qingcheng.dao.SeckillGoodsMapper;
 import com.qingcheng.pojo.order.SeckillStatus;
+import com.qingcheng.pojo.seckill.SeckillGoods;
 import com.qingcheng.pojo.seckill.SeckillOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
@@ -11,25 +13,53 @@ import org.springframework.data.redis.core.RedisTemplate;
 /**
  * 秒杀订单回滚
  */
-public class SeckillOrderMessageListener  implements MessageListener {
+public class SeckillOrderMessageListener implements MessageListener {
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private SeckillGoodsMapper seckillGoodsMapper;
 
     @Override
     public void onMessage(Message message, byte[] bytes) {
         String content = new String(message.getBody());
-        rollbackOrder(JSON.parseObject(content,SeckillStatus.class));
+        rollbackOrder(JSON.parseObject(content, SeckillStatus.class));
     }
 
-    public void rollbackOrder(SeckillStatus seckillStatus){
-        if (seckillStatus==null){
+    public void rollbackOrder(SeckillStatus seckillStatus) {
+        if (seckillStatus == null) {
             return;
         }
 
         //从缓存中找到 用户对应的秒杀订单
         SeckillOrder seckillOrder = (SeckillOrder) redisTemplate.boundHashOps("SeckillOrder").get(seckillStatus.getUsername());
-        if (seckillOrder!=null){
+        if (seckillOrder != null) {
 
+            //先关闭微信支付，成功后在进行回滚
+//            boolean isSuccess = wxxinPayService.closePay(seckillStatus.getOrderId().toString());
+//            if (isSuccess){
+
+
+            //删除用户订单
+            redisTemplate.boundHashOps("SeckillOrder").delete(seckillOrder.getUserId());
+
+
+            //查询对应商品
+            SeckillGoods goods = (SeckillGoods) redisTemplate.boundHashOps("SeckillGoods_" + seckillStatus.getTime()).get(seckillStatus.getGoodsId());
+            // 之前已经被删掉了，需要重新加入到缓存
+            if (goods == null)
+                goods = seckillGoodsMapper.selectByPrimaryKey(seckillStatus.getGoodsId());
+
+            Long seckillGoodsCount = redisTemplate.boundHashOps("SeckillGoodsCount").increment(seckillStatus.getGoodsId(), 1);
+            goods.setStockCount(seckillGoodsCount.intValue());
+
+            redisTemplate.boundHashOps("SeckillGoods_" + seckillStatus.getTime()).put(seckillStatus.getGoodsId(), goods);
+            redisTemplate.boundListOps("SeckillGoodsCountList_" + seckillStatus.getGoodsId()).leftPush(seckillStatus.getGoodsId());
+
+            redisTemplate.boundHashOps("UserQueueCount").delete(seckillStatus.getUsername());
+            redisTemplate.boundHashOps("UserQueueStatus").delete(seckillStatus.getUsername());
+
+//            }
         }
 
     }
